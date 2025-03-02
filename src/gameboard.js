f6a2607eb294371dceec974606e91b8aaec4b4c4
Felix Bruguera <felixbruguera0@gmail.com>
2025-03-02 09:39:55 -0400

    Refactor gameboard.js to use the column_row format of coordinates, Update gameboard tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,27 +3,82 @@ export default class Gameboard {
         this.pieces = []
         this.hits = []
         this.misses = []
+        this.availableBoxes = this.createAvailableBoxes()
+        this.board_size = 10
+    }
+    createAvailableBoxes() {
+        let availableBoxes = []
+        for (let row = 1; row <= 10; row++) {
+            for (let col = 1; col <= 10; col++) {
+                availableBoxes.push(`${col}_${row}`)
+            }
+        }
+        return availableBoxes
     }
     place(ship, startPosition, endPosition) {
-        this.pieces.push({piece: ship, coordinates: this.createCoords(startPosition, endPosition)})
+        let coordinates = this.createCoords(startPosition, endPosition, ship.length)
+        if (coordinates) { 
+            this.removeCoordinates(coordinates)
+            return this.pieces.push({piece: ship, coordinates: coordinates}) }
+        else { return console.log("Invalid position") }
+    }
+    removeCoordinates(coordinates) {
+        this.availableBoxes = this.availableBoxes.filter((box) => !coordinates.includes(box))
     }
     validPlay(playCoords) {
-        if ((playCoords < 1) | (playCoords > 100)) { return false }
-        if (this.hits.includes(playCoords)) { return false }
-        if (this.misses.includes(playCoords)) { return false }
-        return true
+        const col = this.getColumn(playCoords)
+        const row = this.getRow(playCoords)
+        return (
+            col >= 1 && col <= this.board_size &&
+            row >= 1 && row <= this.board_size &&
+            !this.hits.includes(playCoords) &&
+            !this.misses.includes(playCoords)
+        )
     }
-    createCoords(startPosition, endPosition) {
+    getColumn(coordinates) {
+        return parseInt(coordinates.split("_")[0])
+    }
+    getRow(coordinates) {
+        return parseInt(coordinates.split("_")[1])
+    }
+    posiblePositions(ship, startPosition) {
+        const col = this.getColumn(startPosition)
+        const row = this.getRow(startPosition)
+        const shipLength = ship.length - 1
+        let positions = [`${col}_${row+shipLength}`, `${col}_${row-shipLength}`, `${col+shipLength}_${row}`,
+        `${col-shipLength}_${row}`]
+        positions = positions.filter((pos) => this.validPlay(pos))
+        positions = positions.map((endPosition) => this.createCoords(startPosition, endPosition, ship.length))
+        positions = positions.filter((pos) => pos != false)
+        return positions.map((pos) => pos.at(-1))
+
+    }
+    createCoords(startPosition, endPosition, shipSize) {
         let coordinates = []
-        let step = 1
-        if ((endPosition - startPosition) > 4) { step = 10 }
-        for (let i = startPosition; i <= endPosition; i = i + step) {
-            coordinates.push(i)
+        const startCol = this.getColumn(startPosition)
+        const startRow = this.getRow(startPosition)
+        const endCol = this.getColumn(endPosition)
+        const endRow = this.getRow(endPosition)
+        if (startCol == endCol) { this.addVerticalCoords(coordinates, startCol, startRow,endRow, shipSize) }
+        else { this.addHorizontalCoords(coordinates, startCol, endCol,startRow, shipSize) }
+        if (coordinates.every((coord) => this.availableBoxes.includes(coord))) { return coordinates }
+        else { return false }
+    }
+    addVerticalCoords(coordinates, col, startRow, endRow, shipSize) {
+        const step = startRow < endRow ? 1 : -1;
+        for (let i = 0; i < shipSize; i++) {
+            coordinates.push(`${col}_${startRow + (i * step)}`);
         }
-        return coordinates
+    }
+
+    addHorizontalCoords(coordinates, startCol, endCol, row, shipSize) {
+        const step = startCol < endCol ? 1 : -1;
+        for (let i = 0; i < shipSize; i++) {
+            coordinates.push(`${startCol + (i * step)}_${row}`);
+        }
     }
     receiveAttack(playCoords) {
-        if (this.validPlay(playCoords) == false) { return "Invalid Play"}
+        if (this.validPlay(playCoords) == false) { return "Invalid play, try again"}
         let result = this.pieces.find((piece) => piece.coordinates.includes(playCoords))
         if (result == undefined) {
             this.misses.push(playCoords)
@@ -32,8 +87,8 @@ export default class Gameboard {
         else {
             result.piece.hit()
             this.hits.push(playCoords)
-            if (result.piece.isSunk()) { return `Hit, ${result.piece.name} sunk` }
-            return "Hit"
+            if (result.piece.isSunk()) { return `Hit! ${result.piece.name} sunk` }
+            return "Hit!"
         }
     }
     allSunk() {
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -9,65 +9,65 @@ beforeEach(() => {
 
 describe("Places a ship correctly", () => {
     it("When the coordinates are horizontal", () => {
-        gameboard.place(new Ship("Battleship"), 1, 4)
+        gameboard.place(new Ship("Battleship"), "1_1", "4_1")
         let piece = gameboard.pieces[0]
-        expect(piece.coordinates).toContain(1)
-        expect(piece.coordinates).toContain(2)
-        expect(piece.coordinates).toContain(3)
-        expect(piece.coordinates).toContain(4)
+        expect(piece.coordinates).toContain("1_1")
+        expect(piece.coordinates).toContain("2_1")
+        expect(piece.coordinates).toContain("3_1")
+        expect(piece.coordinates).toContain("4_1")
     })
     it("When the coordinates are vertical", () => {
-        gameboard.place(new Ship("Battleship"), 1, 31)
+        gameboard.place(new Ship("Battleship"), "1_1", "1_4")
         let piece = gameboard.pieces[0]
-        expect(piece.coordinates).toContain(1)
-        expect(piece.coordinates).toContain(11)
-        expect(piece.coordinates).toContain(21)
-        expect(piece.coordinates).toContain(31)
+        expect(piece.coordinates).toContain("1_1")
+        expect(piece.coordinates).toContain("1_2")
+        expect(piece.coordinates).toContain("1_3")
+        expect(piece.coordinates).toContain("1_4")
     })
 })
 
 describe("When a play is repeated", () => {
     it("Returns false", () => {
-        gameboard.receiveAttack(5)
-        expect(gameboard.validPlay(5)).toBe(false)
+        gameboard.receiveAttack("5_5")
+        expect(gameboard.validPlay("5_5")).toBe(false)
     })
     it("Does not duplicate the play", () => {
-        gameboard.receiveAttack(5)
-        gameboard.receiveAttack(5)
-        expect(gameboard.misses).toStrictEqual([5])
+        gameboard.receiveAttack("5_5")
+        gameboard.receiveAttack("5_5")
+        expect(gameboard.misses).toStrictEqual(["5_5"])
     })
 })
 describe("When a play is out of the board", () => {
     it("Returns false", () => {
-        expect(gameboard.validPlay(0)).toBe(false)
-        expect(gameboard.validPlay(220)).toBe(false)
+        expect(gameboard.validPlay("0_0")).toBe(false)
+        expect(gameboard.validPlay("9_15")).toBe(false)
     })
 })
 
 describe("Attack resulting in a miss", () => {
     let functionCall
     beforeEach(() => {
-        functionCall = gameboard.receiveAttack(5)
+        functionCall = gameboard.receiveAttack("5_5")
       });
     it("Returns 'Miss'", () => {
         expect(functionCall).toBe("Miss")
     })
     it("Saves the coordinates of the miss", () => {
-        expect(gameboard.misses).toContain(5)
+        expect(gameboard.misses).toContain("5_5")
     })
 })
 
 describe("Attack resulting in a hit", () => {
     let functionCall
     beforeEach(() => {
-        gameboard.place(new Ship("Battleship"), 1, 4)
-        functionCall = gameboard.receiveAttack(3)
+        gameboard.place(new Ship("Battleship"), "1_1", "1_4")
+        functionCall = gameboard.receiveAttack("1_2")
       });
-    it("Returns 'Hit'", () => {
-        expect(functionCall).toBe("Hit")
+    it("Returns 'Hit!'", () => {
+        expect(functionCall).toBe("Hit!")
     })
     it("Saves the coordinates of the hit", () => {
-        expect(gameboard.hits).toContain(3)
+        expect(gameboard.hits).toContain("1_2")
     })
     it("Calls hit() on the hit piece", () => {
         expect(gameboard.pieces[0].piece.hits).toBe(1)
@@ -76,13 +76,30 @@ describe("Attack resulting in a hit", () => {
 
 describe("When all ships are sunk", () => {
     beforeEach(() => {
-        gameboard.place(new Ship("Destroyer"), 1, 3)
+        gameboard.place(new Ship("Destroyer"), "1_1", "1_3")
         for (let i = 1; i < 4; i++) {
-            gameboard.receiveAttack(i)
+            gameboard.receiveAttack(`1_${i}`)
         }
       });
     
     it("Returns true", () => {
         expect(gameboard.allSunk()).toBe(true)
+    })
+})
+
+describe("When posiblePositions is called", () => {
+    it("Returns all posible positions", () => {
+        let call = gameboard.posiblePositions(new Ship("Carrier"), "5_5")
+        expect(call).toContain("5_1")
+        expect(call).toContain("9_5")
+        expect(call).toContain("1_5")
+        expect(call).toContain("5_9")
+    })
+    it("Doesnt return positions out of the board", () => {
+        let call = gameboard.posiblePositions(new Ship("Battleship"), "1_5")
+        expect(call.length).toBe(3)
+        expect(call).toContain("1_2")
+        expect(call).toContain("4_5")
+        expect(call).toContain("1_8")
     })
 })
