10f68ba934323909ad5e6d2fd6fbfdf3d0edf6fc
Felix Bruguera <felixbruguera0@gmail.com>
2025-03-02 13:28:24 -0400

    Add mora game logic and ship placement to index.js, Update the styling for smaller screens

--- src/index.js
+++ src/index.js
@@ -3,63 +3,112 @@ import Computer from "./computer";
 import Gameboard from "./gameboard";
 import Ship from "./ship";
 import GameboardUI from "./gameboardUI";
+import pieceTypes from "./pieceTypes.json" with {type: 'json'}
 import "./index.css"
 
-let player1 = new Player(new Gameboard())
-let player2 = new Computer(new Gameboard(), player1.gameboard)
-let userGameboard = new GameboardUI(player1)
-let computerGameboard = new GameboardUI(player2)
-
-function setDefaults(player) {
-    player.gameboard.place(new Ship("Carrier"), 21, 61)
-    player.gameboard.place(new Ship("Battleship"), 73, 76)
-    player.gameboard.place(new Ship("Destroyer"), 59, 79)
-    player.gameboard.place(new Ship("Submarine"),27, 47)
-    player.gameboard.place(new Ship("Patrol Boat"),14, 15)
-}
-setDefaults(player1)
-setDefaults(player2)
+const SHIP_LIST = Object.keys(pieceTypes)
+let user = new Player(new Gameboard())
+let computer = new Computer(new Gameboard(), user.gameboard)
+let userGameboard = new GameboardUI(user)
+let computerGameboard = new GameboardUI(computer)
+let playerUI = document.querySelector("#player")
+let message = document.querySelector("#message")
+let restart = document.querySelector("#restart")
+let currentShip = 0
 
 function handleResult(result, player) {
-    if (player == "user") {
-        if (result == 'Miss') { setTimeout(() => computerPlays(), 500) }
+    if (player == "User") {
+        if (result == 'Miss') { computerPlays() }
         else { userPlays() }
-        return console.log(result)
     }
     else {
         if (result == 'Miss') { userPlays() }
         else { computerPlays() }
-        return console.log(result)
     }
+    playerUI.textContent = player + ":"
+    return message.textContent = " "+ result
 
+}
+function getDataAttribute(event, dataAttribute) {
+    return event.target.dataset[dataAttribute]
 }
 function userPlays() {
     computerGameboard.board.addEventListener("click", (e) => {
-        let coordinate = e.target.dataset['computerCoordinate']
-        if (coordinate == undefined) { 
-            alert("Invalid play, try again")
-            return userPlays()
-         }
-        coordinate = parseInt(coordinate)
-        let result = player2.gameboard.receiveAttack(coordinate)
+        let coordinate = getDataAttribute(e, 'computerCoordinate')
+        if (coordinate == undefined) { return handleResult("Invalid play, try again", "User") }
+        let result = computer.gameboard.receiveAttack(coordinate)
         computerGameboard.refreshBoard()
-        if (player2.gameboard.allSunk()) { return gameOver("You")}
-        handleResult(result, "user")
+        if (computer.gameboard.allSunk()) { return gameOver("You")}
+        handleResult(result, "User")
     }, {once: true})
 }
 function computerPlays() {
-    let coordinate = player2.play()
-    let result = player1.gameboard.receiveAttack(coordinate)
-    userGameboard.refreshBoard()
-    if (player1.gameboard.allSunk()) { return gameOver("Computer")}
-    handleResult(result, "computer")
+    setTimeout(() => {
+        let coordinate = computer.play()
+        let result = user.gameboard.receiveAttack(coordinate)
+        userGameboard.refreshBoard()
+        if (user.gameboard.allSunk()) { return gameOver("Computer")}
+        handleResult(result, "Computer")
+    }, 1000)
 }
 
 function gameOver(winner) {
-    alert(`Game Over, ${winner} won!`)
+    message.textContent = ''
+    restart.className = ''
+    restart.addEventListener("click", () => playAgain(), {once: true})
+    return playerUI.textContent = `Game Over, ${winner} won!`
 }
 
-userGameboard.paintPieces()
-userPlays()
+function placeShip(ship) {
+    message.textContent = `Place your ${ship.name}`
+    userGameboard.board.addEventListener("click", (e) => {
+        let coordinate = getDataAttribute(e, 'userCoordinate')
+        if (coordinate == undefined || !userGameboard.gameboard.availableBoxes.includes(coordinate)) { 
+            alert("Invalid Selection")
+            return placeShip(ship)
+        }
+        let positions = userGameboard.gameboard.posiblePositions(ship, coordinate)
+        userGameboard.colorBoxes([coordinate], "piece")
+        userGameboard.colorBoxes(positions, "piece-selection")
+        handleSelections(ship, coordinate, positions)
+    }, {once: true})
+}
+
+function handleSelections(ship, startPosition, positions) {
+    userGameboard.board.addEventListener("click", (e) => {
+        let coordinate = getDataAttribute(e, 'userCoordinate')
+        if (positions.includes(coordinate)) {
+            userGameboard.gameboard.place(ship, startPosition, coordinate)
+            userGameboard.colorPieces()
+            currentShip++
+            if (currentShip < 5) { placeShip(new Ship(SHIP_LIST[currentShip]))}
+            else {
+                message.textContent = "Game started!"
+                return userPlays() }
+        }
+        else {
+            alert("Please select a valid box")
+            handleSelections(ship, startPosition, positions)
+        }
+    }, {once: true})
+}
+
+function playAgain() {
+    message.textContent = ''
+    playerUI.textContent = ''
+    restart.className = 'hidden'
+    currentShip = 0
+    user = new Player(new Gameboard())
+    computer = new Computer(new Gameboard(), user.gameboard)
+    userGameboard = new GameboardUI(user)
+    computerGameboard = new GameboardUI(computer)
+    SHIP_LIST.forEach((ship) => computer.placeShip(new Ship(ship)))
+    userGameboard.clearBoard()
+    computerGameboard.clearBoard()
+    placeShip(new Ship("Carrier"))
+}
+
+SHIP_LIST.forEach((ship) => computer.placeShip(new Ship(ship)))
+placeShip(new Ship("Carrier"))
 
 
