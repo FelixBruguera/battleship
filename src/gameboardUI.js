e24809bcdc9b6cf62a9441214e43721d494fc738
Felix Bruguera <felixbruguera0@gmail.com>
2025-03-02 10:40:55 -0400

    Refactor gameboardUI to reduce duplicated code and use clearer function names

--- src/gameboardUI.js
+++ src/gameboardUI.js
@@ -10,36 +10,30 @@ export default class GameboardUI {
         else {return document.querySelector("#computer-board")}
     }
     renderBoard(board) {
-        for (let i = 1; i <= 100; i++) {
-            let box = document.createElement("div")
-            box.setAttribute(`data-${this.type}-coordinate`, i)
-            box.classList.add("box")
-            box.textContent = i
-            board.appendChild(box)
+        for (let row = 1; row <= 10; row++) {
+            for (let col = 1; col <= 10; col++) {
+                let box = document.createElement("div")
+                box.setAttribute(`data-${this.type}-coordinate`, `${col}_${row}`)
+                box.classList.add("box")
+                board.appendChild(box)
+            }
         }
     }
-    paintHits() {
-        this.gameboard.hits.forEach((hit) => {
-            let box = this.board.querySelector(`[data-${this.type}-coordinate = '${hit}']`)
-            box.classList.add("hit")
-        })
-    }
-    paintMisses() {
-        this.gameboard.misses.forEach((miss) => {
-            let box = this.board.querySelector(`[data-${this.type}-coordinate = '${miss}']`)
-            box.classList.add("miss")
+    colorBoxes(boxes, name) {
+        boxes.forEach((box) => {
+            box = this.board.querySelector(`[data-${this.type}-coordinate = '${box}']`)
+            box.classList.add(name)
         })
     }
     refreshBoard() {
-        this.paintHits()
-        this.paintMisses()
+        this.colorBoxes(this.gameboard.hits, "hit")
+        this.colorBoxes(this.gameboard.misses, "miss")
     }
-    paintPieces() {
-        this.gameboard.pieces.forEach((piece) => {
-            piece.coordinates.forEach((coordinate) => {
-                let box = document.querySelector(`div[data-${this.type}-coordinate="${coordinate}"]`)
-                box.classList.add("piece")
-            })
-        })
+    clearBoard() {
+        this.board.querySelectorAll(".box").forEach((box) => box.className = "box")
+    }
+    colorPieces() {
+        this.clearBoard()
+        this.gameboard.pieces.forEach((piece) => this.colorBoxes(piece.coordinates, "piece"))
     }
 }
